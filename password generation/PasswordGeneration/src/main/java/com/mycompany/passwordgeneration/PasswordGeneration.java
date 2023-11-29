63079ecb51dfacf680fb793e150822e58fef12ab
DESKTOP-RCS1AH7\Xeb <ygigamer@gmail.com>
2023-11-29 12:01:02 +0800

    Done

---
+++ password generation/PasswordGeneration/src/main/java/com/mycompany/passwordgeneration/PasswordGeneration.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.passwordgeneration;
+
+/**
+ *
+ * @author Xeb
+ */
+public class PasswordGeneration {
+
+    public static void main(String[] args) {
+        Registration reg = new Registration();
+        reg.setVisible(true);
+    }
+}
